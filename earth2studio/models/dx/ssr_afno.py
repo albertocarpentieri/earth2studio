3a57fe1299959179f07775fde0b2297fc73d8eff
Alberto Carpentieri <acarpentieri@login-eos01.eos.clusters.nvidia.com>
2024-08-26 08:48:32 -0700

    add ssrd afno dx model
    Signed-off-by: Alberto Carpentieri <acarpentieri@login-eos01.eos.clusters.nvidia.com>

---
+++ earth2studio/models/dx/ssr_afno.py
@@ -0,0 +1,32 @@
+# SPDX-FileCopyrightText: Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES.
+# SPDX-FileCopyrightText: All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import zipfile
+from collections import OrderedDict
+from pathlib import Path
+
+import numpy as np
+import torch
+
+from earth2studio.models.auto import AutoModelMixin, Package
+from earth2studio.models.batch import batch_coords, batch_func
+from earth2studio.models.dx.base import DiagnosticModel
+from earth2studio.models.nn.afno_precip import PrecipNet
+from earth2studio.utils import (
+    handshake_coords,
+    handshake_dim,
+)
+from earth2studio.utils.type import CoordSystem
